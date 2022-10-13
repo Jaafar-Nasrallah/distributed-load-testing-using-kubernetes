6b9b13dcaf3d87ba94a991916365094fc019d213
Jaafar-Nasrallah <jaafar_nasrallah@hotmail.com>
2022-10-13 13:52:19 +0300

    updated links

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -29,9 +29,13 @@ class MetricsTaskSet(TaskSet):
     def on_start(self):
         self._deviceid = str(uuid.uuid4())
 
-    @task(1000)
+    @task(200)
     def login(self):
-        self.client.get('/recommend?mwv=true&widgetids=5634837222260736&userid=5e589e7b-7fc9-4858-9a7a-931ff70eeafa&appdomain=dev-blog.cognativex.com&history_postids=124,125&history_adids=-1722605703,137263211,1696517043,-1555703880,-644877927&exc_ads=')
+        self.client.get('/recommend?mwv=true&widgetids=5704070618677248&userid=5c7a3dbf-5f0a-4866-83e9-fbbbe08b8776&appdomain=kooora.com&history_postids=1180081&country_code=LB&device_code=desktop&exc_ads=')
+        self.client.get('/recommend?mwv=true&widgetids=5743010377629696&userid=5c7a3dbf-5f0a-4866-83e9-fbbbe08b8776&appdomain=kooora.com&history_postids=1180081,1179546&country_code=LB&device_code=desktop&exc_ads=')
+        self.client.get('/recommend?mwv=true&widgetids=5749976369987584,5769901083983872&userid=fa8373bb-ccc8-414c-a780-b9e429d50fa8&appdomain=albayan.ae&history_postids=1.4535700&country_code=LB&device_code=desktop&exc_ads=')
+        self.client.get('/recommend?mwv=true&widgetids=5653503041077248,5673817028427776&userid=dae5edc6-2e01-43b0-ada9-7792527a8e27&appdomain=emaratalyoum.com&history_postids=1.1677054&country_code=LB&device_code=desktop&exc_ads=')
+        self.client.get('/recommend?mwv=true&widgetids=5635919931506688,5714101758066688&userid=9ba27121-63cc-4225-a02f-b71608b22012&appdomain=almayadeen.net&history_postids=1638386&country_code=LB&device_code=desktop&exc_ads=')
 
 
 
